67442d0584b3fba6f2e40941903e9f461e0091bf
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2022-04-01 22:29:30 +0700

    update UI for Bug severity section

--- src/app/bug-severity/bug-severity.component.ts
+++ src/app/bug-severity/bug-severity.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bug-severity.component.scss']
 })
 export class BugSeverityComponent implements OnInit {
+  description: String = '';
+  low: String = '0.0';
+  medium: String = '0.0';
+  high: String = '0.0';
+  critical: String = '0.0';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  evaluate() {
+    
   }
 
 }
